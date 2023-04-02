48745add95bd9bc9b7d78d9decf83b1417e1d153
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2023-04-02 20:27:22 +0700

    fix: fix calendar icon

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,42 @@
 class App {
-  constructor() {
-    this.carForm = document.querySelector(".car-search__form");
-    this.driverType = document.getElementById("input-driver-type");
-    this.AvailableAt = document.getElementById("input-available-at");
-    this.PickUpTime = document.getElementById("input-pick-up-time");
-    this.Capacity = document.getElementById("input-capacity");
-    this.searchBtn = document.getElementById("search-btn");
-    this.carCards = document.getElementById("cars-cards");
-  }
+	constructor() {
+		this.carForm = document.getElementById("car-search-form");
+		this.driverType = document.getElementById("input-driver-type");
+		this.AvailableAt = document.getElementById("input-available-at");
+		this.PickUpTime = document.getElementById("input-pick-up-time");
+		this.Capacity = document.getElementById("input-capacity");
+		this.searchBtn = document.getElementById("search-btn");
+		this.carCards = document.getElementById("cars-cards");
+	}
 
-  async init() {
-    await this.load();
-    this.searchBtn.onclick = this.search;
-  }
+	async init() {
+		await this.load();
+		this.searchBtn.onclick = this.search;
+	}
 
-  search = async () => {
-    this.clear();
-    
-    const driverType = this.driverType.value;
-    const availableAt = this.AvailableAt.value;
-    const pickUpTime = this.PickUpTime.value;
-    const capacity = this.Capacity.value;
-    
-    if (!driverType || !availableAt || !pickUpTime || !capacity) {
-      alert("mohon isi semua");
-      this.resetForm();
-      this.init()
-      this.run();
-    } else {
-      const dateTime = new Date(`${availableAt} ${pickUpTime}`);
-      this.filterCar(dateTime, capacity);
-    }
-  }
+	search = async () => {
+		this.clear();
+
+		const driverType = this.driverType.value;
+		const availableAt = this.AvailableAt.value;
+		const pickUpTime = this.PickUpTime.value;
+		const capacity = this.Capacity.value;
+
+		if (!driverType || !availableAt || !pickUpTime || !capacity) {
+			alert("Anda diharapkan mengisi semua form!");
+			this.resetForm();
+			this.init().then(this.run);
+		} else {
+			const dateTime = new Date(`${availableAt} ${pickUpTime}`);
+			this.filterCar(dateTime, capacity);
+		}
+	}
 
 	async load() {
 		const cars = await Binar.listCars();
 		Car.init(cars);
 	}
-  
+
 	run = () => {
 		Car.list.forEach((car) => {
 			const node = document.createElement("div");
@@ -46,29 +45,29 @@ class App {
 		});
 	};
 
-  clear = () => {
-    let child = this.carCards.firstElementChild;
+	clear = () => {
+		let child = this.carCards.firstElementChild;
 
-    while (child) {
-      child.remove();
-      child = this.carCards.firstElementChild;
-    }
-  };
+		while (child) {
+			child.remove();
+			child = this.carCards.firstElementChild;
+		}
+	};
 
-  resetForm = () => {
-    this.carForm.reset();
-  }
+	resetForm = () => {
+		this.carForm.reset();
+	}
 
-  filterCar = async (dateTime, capacity) => {
-    const cars = await Binar.listCars((car) => {
-      const available = car.available
-      const dateFilter = car.availableAt >= dateTime;
-      const capacityFilter = car.capacity >= capacity;
-      return available && dateFilter && capacityFilter;
-    });
-    
-    Car.init(cars);
-    this.run();
-    this.resetForm();
-  }
+	filterCar = async (dateTime, capacity) => {
+		const cars = await Binar.listCars((car) => {
+			const available = car.available
+			const dateFilter = car.availableAt >= dateTime;
+			const capacityFilter = car.capacity >= capacity;
+			return available && dateFilter && capacityFilter;
+		});
+
+		Car.init(cars);
+		this.run();
+		this.resetForm();
+	}
 }
--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
-const dir = path.resolve();
-const staticFilePath = path.join(dir, "public");
+const port = 8000;
+const publicDirectory = path.join(__dirname, '../public');
 
-app.use(express.static(staticFilePath));
+app.use(express.static(publicDirectory));
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(staticFilePath, "index.html"));
+app.get('/', (req, res) => {
+	res.status(200)
+	res.sendFile(path.join(publicDirectory, 'index.html'));
 });
 
-app.get("/cars", (req, res) => {
-  res.status(200).sendFile(path.join(staticFilePath, "find-car.html"));
+app.get('/cars', (req, res) => {
+	res.status(200)
+	res.sendFile(path.join(publicDirectory, 'find-car.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server Listen on port http://localhost:${port}`);
+	console.log(`Server telah berhasil berjalan pada port http://localhost:${port}`);
 });
